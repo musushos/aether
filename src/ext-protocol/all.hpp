43c61460e5898e03534aac00869026a1d91ce011
hussein jaafar <vaxpeco@gmail.com>
2026-05-20 09:21:26 +0300

    switcher bit

--- src/ext-protocol/all.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "aether-ipc.hpp"
-#include "ext-workspace.hpp"
-#include "foreign-toplevel.hpp"
-#include "tearing.hpp"
-#include "text-input.hpp"
--- src/fetch/fetch.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "client.hpp"
-#include "common.hpp"
-#include "monitor.hpp"
